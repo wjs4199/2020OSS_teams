30277adebf943c67716dfee5789d2f8ef19bbd41
YeJinYeon <21900441@handong.edu>
2020-05-04 23:30:03 +0900

    Update book.h

--- book.h
+++ book.h
@@ -1,7 +1,9 @@
 #ifndef BOOK
 #define BOOK
 
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef struct{
 	char name[30];
@@ -10,11 +12,12 @@ typedef struct{
 	int star;
 }Book; //Book 구조체 정의
 
+int selectMenu();
 
-
-
-
-
-
+int CreateBook(Book *b);
+void readBook(Book b);
+void listBook(Book *b, int count);
+int updateBook(Book *b);
+int deleteBook(Book *b);
 
 #endif
