918d6b2263c267b8f3ef3afd5beb927126f3f504
Jeonyeeun <wjs4199@naver.com>
2020-05-12 22:19:22 +0900

    Add searchstar, searchfunction in manager.c

--- manager.c
+++ manager.c
@@ -76,4 +76,31 @@ void searchPrice(Book* b,int count){
   if(scount==0) printf("=> 검색된 데이터 없음!");
   printf("\n");
 }
+//원하는 평점 이상의 책을 찾아주는 함수
+void searchStar(Book* b,int count){
+  scount=0;//데이터의 존재유무 가리는 변수
+  printf("원하는 평점? (0~5)");
+  scanf("%d",&starnum);
+  for(int i=0;i<count;i++){
+    if(b[i].cost !=-1)
+      if(b[i].star>=starnum){
+        printf("%2d ",i+1);
+        readBook(b[i]);
+        scount++;
+      }
+  }
+  if(scount==0) printf("=> 검색된 데이터 없음!");
+  printf("\n");
+}
 
+//검색 기능을 하는 함수
+void searchFunction(Book* b,int count){
+  printf("=== 검색기능 ===\n1. 제품명 검색\n2. 제품가격대 검색\n3. 제품평점 검색
+\n0. 취소\n");
+  printf("원하는 번호는?");
+  scanf("%d",&searchnumber);
+  if(searchnumber==1) searchName(b,count);
+  else if(searchnumber==2) searchPrice(b, count);
+  else if(searchnumber==3) searchStar(b, count);
+  else printf("취소되었습니다!");
+}
